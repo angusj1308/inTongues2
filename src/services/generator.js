3b1a786ba394eee5ceceeae93d6616ee2beda140
angusj1308 <angusmacrae96@gmail.com>
2025-11-25 04:28:48 +1000

    Point generator service to local emulator

--- src/services/generator.js
+++ src/services/generator.js
@@ -1,12 +1,15 @@
 export const generateStory = async (params) => {
   try {
-    const response = await fetch('/api/generate', {
+    const response = await fetch(
+      'http://127.0.0.1:5001/intongues2/us-central1/generateStory/api/generate',
+      {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify(params),
-    })
+      }
+    )
 
     if (!response.ok) {
       const errorPayload = await response.json().catch(() => ({}))

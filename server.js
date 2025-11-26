bfec2f47ed05696b32f05fc81bdb1846955ca9b8
angusj1308 <angusmacrae96@gmail.com>
2025-11-26 20:50:54 +1000

    Reorder dotenv initialization

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 import express from 'express'
 import dotenv from 'dotenv'
+dotenv.config()
 import OpenAI from 'openai'
 
 const client = new OpenAI({ apiKey: process.env.OPENAI_API_KEY })
-dotenv.config()
 
 const app = express()
 app.use(express.json())

d69b2f6919560080b09af396f6833cf6bfd3ef26
angusj1308 <angusmacrae96@gmail.com>
2025-11-30 12:17:24 +1000

    Use createRequire for service account import

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ import fs from 'fs/promises'
 import pdfParse from 'pdf-parse'
 import EPub from 'epub2'
 import admin from 'firebase-admin'
-import serviceAccount from './serviceAccountKey.json' assert { type: 'json' }
+import { createRequire } from 'module'
+const require = createRequire(import.meta.url)
+const serviceAccount = require('./serviceAccountKey.json')
 dotenv.config()
 import OpenAI from 'openai'
 
